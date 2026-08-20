7ca8511840e9593c2f3b69f7a33e67f29a88090b
agilfebripradana123 <agilfebripradana123@gmail.com>
2026-08-20 22:20:35 +0700

    chore: initialize Laravel backend

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, SoftDeletes, Notifiable;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'role_id',
+        'username',
+        'name',
+        'email',
+        'password',
+        'photo',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'is_active' => 'boolean',
+        ];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+}
